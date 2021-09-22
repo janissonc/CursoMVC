4e5a28bd8cf11462a06418833509fb41794a1c1f
Mylenna <Mylenna@DESKTOP-SM0KMME>
2021-09-22 14:13:07 -0300

    Adicionar arquivos de projeto.

---
+++ CursoMVC/Models/Categoria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Categoria
+    {
+        public int id { get; set; }
+        public string description { get; set; }
+        
+
+    }
+}
---
+++ CursoMVC/Models/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Context: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+        {
+            optionsBuilder.UseSqlServer("Data Source=.\\SQLEXPRESS;Initial Catalog=CursoMvc;Integrated Security=True;User ID=sa;Password=1234;");
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+    }
+}
