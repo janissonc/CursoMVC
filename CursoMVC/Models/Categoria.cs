cfff965db972ec77e410dbdc7242218fab4e86dc
Janisson <janisson.bass@gmail.com>
2021-09-22 16:51:07 -0300

    Finalizado o projeto

--- CursoMVC/Models/Categoria.cs
+++ CursoMVC/Models/Categoria.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,9 @@ namespace CursoMVC.Models
     public class Categoria
     {
         public int id { get; set; }
+
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "O campo descrição é obrigatório!!")]
         public string description { get; set; }
         
         public List<Produto> Produtos { get; set; }
--- CursoMVC/Models/Produto.cs
+++ CursoMVC/Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,8 +10,11 @@ namespace CursoMVC.Models
     {
         public int Id { get; set; }
 
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage ="O campo descrição é obrigatório!!")]
         public string Descricao { get; set; }
 
+        [Required(ErrorMessage = "O campo quantidade é obrigatório!!")]
         public int Quantidade { get; set; }
 
         public int CategoriaId { get; set; }
