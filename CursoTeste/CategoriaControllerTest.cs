093d00f5b9aa9c0ae6852ad516fce0181093ad4c
Janisson <janisson.bass@gmail.com>
2021-09-23 08:11:19 -0300

    Adicionado Sulution de teste.

---
+++ CursoTeste/CategoriaControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using CursoMVC.Models;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoTeste
+{
+    public class CategoriaControllerTest
+    {
+        private readonly Mock<DbSet<Categoria>> _mockSet;
+        private readonly Mock<DbSet<Context>> _mockContext;
+        private readonly Categoria _categoria;
+
+        public CategoriaControllerTest() { }
+    }
+}
