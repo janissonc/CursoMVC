6c8cb35567a1a9af007b067659c3852e8d2f3663
Janisson <janisson.bass@gmail.com>
2021-09-24 08:25:24 -0300

    Removido projeto de teste

--- CursoTeste/CategoriaControllerTest.cs
+++ CursoTeste/CategoriaControllerTest.cs
@@ -1,9 +1,12 @@
-﻿using CursoMVC.Models;
+﻿using CursoAPI.Controllers;
+using CursoMVC.Models;
 using Microsoft.EntityFrameworkCore;
 using Moq;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace CursoTeste
 {
@@ -13,6 +16,24 @@ namespace CursoTeste
         private readonly Mock<DbSet<Context>> _mockContext;
         private readonly Categoria _categoria;
 
-        public CategoriaControllerTest() { }
+        public CategoriaControllerTest() 
+        {
+            _mockSet = new Mock<DbSet<Categoria>>();
+            _mockContext = new Mock<DbSet<Context>>();
+            _categoria = new Categoria { id = 1 , description = "teste description"};
+
+            //_mockContext.Setup(m => m.Categorias).Returns(_mockSet.Object);
+            //_mockContext.Setup(m => m.Categorias.FindAsync(1)).ReturnsAsync(_categoria);
+        }
+
+        [Fact]
+        public async Task Get_Categoria() 
+        {
+            //var services = new CategoriasController(_mockContext.Object);
+
+            //await services.GetCategoria(1);
+
+            _mockSet.Verify(m => m.FindAsync(1),Times.Once());
+        }
     }
 }
